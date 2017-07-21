6674a6aedd9063864129d0043664fca710d74c01
patrik <patrik@patrik.hr>
2017-07-22 01:32:30 +0200

    testing

---
+++ test/testclass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by patrik on 22.07.17.
+//
+
+#include "testclass.h"
---
+++ test/testclass.h
@@ -0,0 +1,14 @@
+//
+// Created by patrik on 22.07.17.
+//
+
+#ifndef PHYSICSENGINE2_TESTCLASS_H
+#define PHYSICSENGINE2_TESTCLASS_H
+
+
+class testclass {
+
+};
+
+
+#endif //PHYSICSENGINE2_TESTCLASS_H
